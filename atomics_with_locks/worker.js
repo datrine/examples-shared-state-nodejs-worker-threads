423db4a8ab68fbc5a4484274ad3452160f41e037
datrine <trinitietp@gmail.com>
2026-04-05 21:05:05 +0100

    fix: add try finally to worker acquiring lock

--- atomics_with_locks/worker.js
+++ atomics_with_locks/worker.js
@@ -1,13 +1,21 @@
-const { workerData } = require('worker_threads');
-const { acquireLock, releaseLock } = require('./lock_utils');
-const worker_id=workerData.id
+const { workerData } = require("worker_threads");
+const { acquireLock, releaseLock } = require("./lock_utils");
+const worker_id = workerData.id;
 const view = new Int32Array(workerData.sab);
-const LOCK_IDX    = 0;
+const LOCK_IDX = 0;
 const COUNTER_IDX = 1;
-for(let i = 0; i < 1000; i++){
+for (let i = 0; i < 1000; i++) {
   acquireLock(view, LOCK_IDX);
-  view[COUNTER_IDX] += 1; // writes to shared memory
-  console.log(`worker id: ${worker_id} view[COUNTER_IDX]: ${view[COUNTER_IDX]}`);
-  releaseLock(view, LOCK_IDX);
+  try {
+    view[COUNTER_IDX] += 1; // writes to shared memory
+    console.log(
+      `worker id: ${worker_id} view[COUNTER_IDX]: ${view[COUNTER_IDX]}`,
+    );
+  } catch (error) {
+    // Handle error if needed
+    console.error(`Error in worker ${worker_id}:`, error);
+  } finally {
+    // Ensure the lock is released even if an error occurs
+    releaseLock(view, LOCK_IDX);
+  }
 }
-
